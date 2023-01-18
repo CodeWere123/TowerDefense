1ab46b0d4f9c055586f9a5ffa60371ec68880b96
CodeWere123 <alekspost700@gmail.com>
2023-01-18 19:11:54 +0300

    закинул норм полет камня по прямой к врагу

--- projectail.py
+++ projectail.py
@@ -2,6 +2,7 @@ import pygame
 from load_image import load_image
 from SpriteGroups import projectile_group, all_sprites
 import sys
+import math
 class Projectail(pygame.sprite.Sprite):
     def __init__(self, pos, target_pos, damage, speed):
         """
@@ -23,6 +24,11 @@ class Projectail(pygame.sprite.Sprite):
         self.damage = damage
         self.i_index = 0
         self.target_x, self.target_y = target_pos
+        dx = self.target_x - self.x
+        dy = self.target_y - self.y
+        distance = math.sqrt(dx ^ 2 + dy ^ 2)
+        self.speed_x = self.speed * dx / distance
+        self.speed_y = self.speed * dy / distance
 
     def move(self):
         if abs(self.target_x - self.rect.x) + abs(self.target_y - self.rect.y) < 5:
@@ -30,14 +36,14 @@ class Projectail(pygame.sprite.Sprite):
             if self.i_index == 0:
                 self.i_index += 1
         if self.i_index == 0:
-            if self.rect.x < self.target_x:
-                self.rect.x += self.speed
+            if self.x < self.target_x:
+                self.x += self.speed_x
             else:
-                self.rect.x -= self.speed
-            if self.rect.y < self.target_y:
-                self.rect.y += self.speed
+                self.rect.x -= self.speed_x
+            if self.y < self.target_y:
+                self.y += self.speed_y
             else:
-                self.rect.y -= self.speed
+                self.y -= self.speed_y
         else:
             if self.tick > self.delay:
                 self.tick = 0
@@ -47,26 +53,28 @@ class Projectail(pygame.sprite.Sprite):
                 else:
                     projectile_group.remove(self)
                     self.kill()
+                    Projectail((100, 100), (400, 300), 1, 0.6)
 
             else:
                 self.tick += 1
+        self.rect.x, self.rect.y = self.x, self.y
 
-#if __name__ == '__main__':
-#    pygame.init()
-#    size = width, height = 1000, 500
-#    screen = pygame.display.set_mode(size)
-#
-#    running = True
-#    test = Projectail((100, 100), (400, 300), 1, 1)
-#    while running:
-#        screen.fill("black")
-#        for stone in projectile_group:
-#            stone.move()
-#        all_sprites.draw(screen)
-#        for event in pygame.event.get():
-#            if event.type == pygame.QUIT:
-#                running = False
-#        pygame.display.flip()
-#    pygame.quit()
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 1000, 500
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    test = Projectail((100, 100), (400, 300), 1, 0.01)
+    while running:
+        screen.fill("black")
+        for stone in projectile_group:
+            stone.move()
+        all_sprites.draw(screen)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
+    pygame.quit()
 
 
